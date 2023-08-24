ae02b8778e6ef7d89db06747a34df664185d14d1
sinsar11456 <sinsarsanu111@gmail.com>
2023-08-24 22:37:25 +0530

    single product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 
 import Home from "./pages/Home";
 import ProductList from "./components/ProductList";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <div>
-      <ProductList />
+      <Product />
     </div>
   );
 }
---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,164 @@
+import { styled } from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import { Add, Remove } from "@mui/icons-material";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  font: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+ display: flex;
+ align-items: center;
+ font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+padding: 15px;
+border: 2px solid teal;
+background-color: white;
+cursor: pointer;
+font-weight: 500;
+
+&:hover{
+    background-color: f8f4f4;
+}
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://www.bardot.com/dw/image/v2/BCGZ_PRD/on/demandware.static/-/Sites-bardot-master-catalog/default/dw8dd8a9f9/images/hi-res/bardot/91092PB/91092PB_164010_TF_02.png?sw=1000&sh=1500&sm=fit" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Originally from Latin, Lorem ipsum has no intelligible meaning. It
+            is simply a display of letters to be viewed as a sample with given
+            graphical elements in a file. "Lipsum" (a portmanteau of lorem and
+            ipsum) generators are commonly used to form generic
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
