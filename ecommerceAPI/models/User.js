90c4f51a1f22daf4b83a7aac52e81cef75dd04a4
sinsar11456 <sinsarsanu111@gmail.com>
2023-09-20 09:11:59 +0530

    Mongo DB auth

--- ecommerceAPI/models/User.js
+++ ecommerceAPI/models/User.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const { boolean } = require("webidl-conversions");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: string, required: true, unique: true },
-  email: { type: string, required: true, unique: true },
-  password: { type: string, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   isAdmin: {
     type: Boolean,
     default: false,
   },
-}
+},
 {timestamps:true}
 );
 
-model.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User",UserSchema);
--- ecommerceAPI/routes/auth.js
+++ ecommerceAPI/routes/auth.js
@@ -1,17 +1,48 @@
 const router = require("express").Router();
-const User = require("../models/User")
-
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
-router.post("/register", (req,res)=> {
-     const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-     });
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//LOGIN
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(401).json("wrong credentials!");
+      return;
+    }
 
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const password = hashedPassword.tostring(CryptoJS.enc.utf8);
+    if (password !== req.body.password) {
+      res.status(401).json("wrong credentials!");
+      return;
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = router 
+module.exports = router;
