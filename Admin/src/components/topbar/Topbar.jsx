4360a5b813a5076a67e42c374b8e8812011cfc83
sinsar11456 <sinsarsanu111@gmail.com>
2023-10-05 17:11:06 +0530

    topbar finish

---
+++ Admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./topbar.css";
+import { Language, NotificationsNone, Settings } from "@mui/icons-material";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">sinsadmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconsContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconsContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconsContainer">
+            <Settings />
+          </div>
+          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvathar" />
+        </div>
+      </div>
+    </div>
+  );
+}
