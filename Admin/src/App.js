0f33766710049a30143663c8cf51fc03b9d49262
sinsar11456 <sinsarsanu111@gmail.com>
2023-10-08 08:42:21 +0530

    react sidebar menu

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -1,9 +1,16 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ Admin/src/components/featuredinfo/Featuredinfo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Featuredinfo() {
+  return (
+    <div className="featured">
+      <div className="featuredItems">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+            <span className="featuredMoney">$2,33</span>
+            <span className="featuredMoneyRate">-11.3</span>
+        </div>
+      </div>
+    </div>
+  );
+}
