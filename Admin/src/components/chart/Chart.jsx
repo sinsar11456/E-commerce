0b02787f846b6e98892c550ce8fe086d8c04b657
sinsar11456 <sinsarsanu111@gmail.com>
2023-10-10 12:58:51 +0530

    chart full

--- Admin/src/components/chart/Chart.jsx
+++ Admin/src/components/chart/Chart.jsx
@@ -1,4 +1,5 @@
 import "./chart.css";
+import "../../dummyData";
 import {
   LineChart,
   Line,
@@ -8,65 +9,16 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-export default function Chart() {
-  const data = [
-    {
-      name: "Jan",
-      "Active User": 4000,
-    },
-    {
-      name: "Feb",
-      "Active User": 3000,
-    },
-    {
-      name: "Mar",
-      "Active User": 000,
-    },
-    {
-      name: "Apr",
-      "Active User": 2780,
-    },
-    {
-      name: "May",
-      "Active User": 1890,
-    },
-    {
-      name: "June",
-      "Active User": 2390,
-    },
-    {
-      name: "July",
-      "Active User": 3490,
-    },
-    {
-      name: "Aug",
-      "Active User": 3490,
-    },
-    {
-      name: "Sep",
-      "Active User": 3490,
-    },
-    {
-      name: "Oct",
-      "Active User": 3490,
-    },
-    {
-      name: "Nov",
-      "Active User": 3490,
-    },
-    {
-      name: "Dec",
-      "Active User": 3490,
-    },
-  ];
-
+export default function Chart({title,data, dataKey , grid}) {
   return (
     <div className="chart">
-      <h3 className="chartTitle">User Analytics</h3>
+      <h3 className="chartTitle">{title}</h3>
       <ResponsiveContainer width="100%" aspect={4 / 1}>
-        <LineChart date={data}>
+        <LineChart data={data}>
           <XAxis dataKey="name" stroke="#5550bd" />
-          <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+            <Tooltip />
+            { grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5"/>}
         </LineChart>
       </ResponsiveContainer>
     </div>
--- Admin/src/components/home/Home.jsx
+++ Admin/src/components/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./home.css";
 import Featuredinfo from "../featuredinfo/Featuredinfo";
 import Chart from "../chart/Chart";
+import { userData } from "../../dummyData";
 
 export default function Home() {
     return(
     <div className="home">
         <Featuredinfo/> 
-        <Chart/>
+        <Chart data={userData} title= "User Analytics" grid dataKey="Active User"/>
+
     </div>
     );
 }
