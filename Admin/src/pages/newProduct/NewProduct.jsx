f7b4afdb97c51434be900cb007a1bce201dd4885
sinsar11456 <sinsarsanu111@gmail.com>
2023-11-27 10:49:17 +0530

    handle two multiple objects in one Store

--- Admin/src/pages/newProduct/NewProduct.jsx
+++ Admin/src/pages/newProduct/NewProduct.jsx
@@ -5,33 +5,42 @@ export default function NewProduct() {
   const [inputs, setInputs] = useState({});
   const [file, setFile] = useState(null);
   const [cat,setCat] = useState([])
+
+  const handleChange = (e)=>{
+    setInputs(prev=>{
+      return {...prev, [e.target.name]:e.target.value}
+    })
+  }
+  const handleCat = (e)=>{
+     setCat(e.target.value.split(","));
+  };
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
-          <label>Image</label>
-          <input type="file" id="file" />
+          <label>Image</label> 
+          <input type="file" id="file" onChange={e=>setFile(e.target.files)[0]} />
         </div>
         <div className="addProductItem">
           <label>Title</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <input name="title" type="text" placeholder="Apple Airpods" />
         </div>
         <div className="addProductItem">
           <label>Description</label>
-          <input type="text" placeholder="Description..." />
+          <input name="desc" type="text" placeholder="Description..." onChange={handleChange} />
         </div>
         <div className="addProductItem">
           <label>Price</label>
-          <input type="text" placeholder="100" />
+          <input name="price" type="number" placeholder="100" onChange={handleChange}/>
         </div>
         <div className="addProductItem">
           <label>categories</label>
-          <input type="text" placeholder="jeans,skirt" />
+          <input type="text" placeholder="jeans,skirt" onChange={handleCat}/>
         </div>
-        <div className="addProductItem">
+        <div name="stock" className="addProductItem">
           <label>Stock</label>
-          <select>
+          <select onChange={handleChange}>
             <option value="true">Yes</option>
             <option value="false">NO</option>
           </select>
